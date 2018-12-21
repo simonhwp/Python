295007fae9554467811e8767083e7feb8320c1be
simon <simonhwp@gmail.com>
2018-12-22 00:42:23 +0800

    new change

--- dict-list.py
+++ dict-list.py
@@ -1,21 +1,71 @@
 # 两个字典合并
-# d1 = {'apple': 12, 'banana': 2}
-# d2 = {'orange': 2, 'banana': 4}
-# # d3 = dict(d1, **d2)
-# '''
-# z = d1.copy()
-# z.update(d2)
-# '''
-# d3 = {**d1, **d2}  #python3.5 PEP448 新的使用方法
-# print(d3)
+d1 = {'apple': 12, 'banana': 2}
+d2 = {'orange': 2, 'banana': 4}
+# d3 = dict(d1, **d2)
+'''
+z = d1.copy()
+z.update(d2)
+'''
+d3 = {**d1, **d2}  #python3.5 PEP448 新的使用方法
+print(d3)
 
 # 创建字典，并根据字母顺序输出键值
-# d1 = {'apple': 14, 'oragne': 14, 'banana': 5}
-# print(sorted(d1.keys()))
-# print([(i, d1[i]) for i in sorted(d1.keys())])
+d1 = {'apple': 14, 'oragne': 14, 'banana': 5}
+print(sorted(d1.keys()))
+print([(i, d1[i]) for i in sorted(d1.keys())])
 
 # 将两个长度相同的列表合并成一个字典
 l1 = [1, 2, 3, 4, 5]
 l2 = ['a', 'b', 'c', 'd', 'e']
 d1 = dict(zip(l1, l2))
 print(d1)
+
+# 翻译
+srcstr = 'abc'
+ssrc = set(srcstr)
+dststr = 'mno'
+sdst = set(dststr)
+string = 'abcdef'
+sstr = set(string)
+difstr = sstr - ssrc
+sstr.difference_update(ssrc)
+print(sstr)
+_newstr = sdst | difstr
+print(_newstr)
+
+# 加密, 用输入的字符的第13位数字来替代
+def string_to_rot13(inpstr):
+    result = []
+    n = 13
+    newinput = list(inpstr)
+    lstr = len(newinput)  
+    for i in range(lstr):
+        if newinput[i].isupper():
+            if ord(newinput[i]) < 91-n:
+                c = chr(ord(newinput[i]) + n)
+                result.append(c)
+            else:
+                c = chr(ord(newinput[i]) + n - 26)
+                result.append(c)
+        elif newinput[i].islower():
+            if ord(newinput[i]) < 123-n:
+                c = chr(ord(newinput[i]) + n)
+                result.append(c)
+            else:
+                c = chr(ord(newinput[i]) + n - 26)
+                result.append(c)
+        else:
+            c = newinput[i]
+            result.append(c)
+    return result
+
+youstr = input('please enter sting to rot13:')
+print('Your string to en was:', youstr)
+newstr = list(youstr.split())
+lstr = len(newstr)
+outstr = []
+for i in range(lstr):
+    newlstr = string_to_rot13(newstr[i])
+    outstr.append(''.join(newlstr))
+    print(outstr)
+print('The rot13 string is:', ' '.join(outstr))
--- list-str-tuple.py
+++ list-str-tuple.py
@@ -1,119 +1,118 @@
 # 输入一串数字，并从大到小排序
-# num = input('Enter New num:')
+num = input('Enter New num:')
 
-# new_num = list(map(int, num.strip().split()))
-# print(new_num)
-# # new_num.sorted(reverse=False)
-# print(sorted(new_num, reverse=True))
-# print(new_num.sort(reverse=True))
+new_num = list(map(int, num.strip().split()))
+print(new_num)
+# new_num.sorted(reverse=False)
+print(sorted(new_num, reverse=True))
+print(new_num.sort(reverse=True))
 
 # 测试得分放到一个列表中，并求出平均值
-# num = input('Enter New num:')
-# new_num = list(map(int, num.strip().split()))
-# total = 0
-# for i in range(len(new_num)):
-#     total += new_num[i]
-# avg_num = total / len(new_num)
-# print('The avg is ', avg_num)
+num = input('Enter New num:')
+new_num = list(map(int, num.strip().split()))
+total = 0
+for i in range(len(new_num)):
+    total += new_num[i]
+avg_num = total / len(new_num)
+print('The avg is ', avg_num)
 
 # 每次向前向后都显示一个字符串的一个字符
-# s1 = input('Enter New string:')
+s1 = input('Enter New string:')
 
-# for i in range(len(s1)):
-#     print(s1[i])  #从头开始输出字符
-# s2 = s1[::-1]
-# for j in range(len(s2)):
-#     print(s2[j])  #从尾开始输出字符
+for i in range(len(s1)):
+    print(s1[i])  #从头开始输出字符
+s2 = s1[::-1]
+for j in range(len(s2)):
+    print(s2[j])  #从尾开始输出字符
 
 # 通过扫描来判断两个字符串是否匹配
-
-# s1, s2 = input('请输入两个字符串，以空格相隔：').split()
-# if (len(s1) != len(s2)):
-#     print('两个字符串不相符')
-# else:
-#     tag = True
-#     for i in range(len(s1)):
-#         if s1[i] != s2[i]:
-#             tag = False
-#             break
-#     if (tag):
-#         print('两个字符串相符')
-#     else:
-#         print('两个字符串不相符')
+s1, s2 = input('请输入两个字符串，以空格相隔：').split()
+if (len(s1) != len(s2)):
+    print('两个字符串不相符')
+else:
+    tag = True
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            tag = False
+            break
+    if (tag):
+        print('两个字符串相符')
+    else:
+        print('两个字符串不相符')
 
 # 接受一个字符串，去年它的前面和后面的空格
-# s1 = input('输入一个字符串：')
+s1 = input('输入一个字符串：')
 
-# start_index = 0
-# end_index = len(s1)
+start_index = 0
+end_index = len(s1)
 
-# if ' ' == s1[0]:
-#     start_index = 1
+if ' ' == s1[0]:
+    start_index = 1
 
-# if ' ' == s1[end_index - 1]:
-#     end_index -= 1
-# print(s1[start_index:end_index])
-# print(len(s1[start_index:end_index]), len(s1), start_index, end_index)
+if ' ' == s1[end_index - 1]:
+    end_index -= 1
+print(s1[start_index:end_index])
+print(len(s1[start_index:end_index]), len(s1), start_index, end_index)
 
 # 给出一个整型值，返回代表该值的英文
-# num = input('请输入一个整型值：')
-# inum = int(num)
-# if 0 > inum:
-#     print('数值不能为负。')
-# if 1000 < inum:
-#     print('数值不能超过1000。')
-# dnum = [
-#     'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
-#     'nine'
-# ]
-# restr = ''
-# istrlen = len(num)
-# if 1 == istrlen:
-#     restr = dnum[int(num)]
-#     print(restr)
-# else:
-#     for i in range(istrlen):
-#         temp = dnum[int(num[i])]
-#         if i != istrlen - 1:
-#             restr += temp + '-'
-#         else:
-#             restr += temp
+num = input('请输入一个整型值：')
+inum = int(num)
+if 0 > inum:
+    print('数值不能为负。')
+if 1000 < inum:
+    print('数值不能超过1000。')
+dnum = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+    'nine'
+]
+restr = ''
+istrlen = len(num)
+if 1 == istrlen:
+    restr = dnum[int(num)]
+    print(restr)
+else:
+    for i in range(istrlen):
+        temp = dnum[int(num[i])]
+        if i != istrlen - 1:
+            restr += temp + '-'
+        else:
+            restr += temp
 
-#     print(restr)
+    print(restr)
 
 # 字符串大小反转
-# s1 = input('请输入一个字符串：')
-# s1_list = list(s1)
-# ls1 = len(s1)
-# re_s1 = ''
-# print(s1_list)
-# for i in range(ls1):
-#     temp = ''
-#     if True == s1_list[i].islower():
-#         temp = s1_list[i].upper()
-#     elif True == s1_list[i].isupper():
-#         temp = s1_list[i].lower()
-#     else:
-#         temp = s1_list[i]
-#     re_s1 += temp
+s1 = input('请输入一个字符串：')
+s1_list = list(s1)
+ls1 = len(s1)
+re_s1 = ''
+print(s1_list)
+for i in range(ls1):
+    temp = ''
+    if True == s1_list[i].islower():
+        temp = s1_list[i].upper()
+    elif True == s1_list[i].isupper():
+        temp = s1_list[i].lower()
+    else:
+        temp = s1_list[i]
+    re_s1 += temp
 
-# print(re_s1)
+print(re_s1)
 
 # 改错
-# num_str = input('Enter a number:')
-# num_num = int(num_str)
-# # fac_list = range(1, num_num + 1) 无法直接range赋值List
-# fac_list = list(range(1, num_num + 1))
-# print('befor list:', fac_list)
-# i = 0
-# while i < len(fac_list):
-#     if num_num % fac_list[i] == 0:
-#         del fac_list[i]
-#     i = i + 1
-# print('after list:', fac_list)
+num_str = input('Enter a number:')
+num_num = int(num_str)
+# fac_list = range(1, num_num + 1) 无法直接range赋值List
+fac_list = list(range(1, num_num + 1))
+print('befor list:', fac_list)
+i = 0
+while i < len(fac_list):
+    if num_num % fac_list[i] == 0:
+        del fac_list[i]
+    i = i + 1
+print('after list:', fac_list)
 
-# find(), index()
-# print(list(range(10, 0, -1)))
+find(), index()
+print(list(range(10, 0, -1)))
 
 # 随机数，石头、剪刀、布
 import random
